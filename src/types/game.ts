f4261959fc970833134337e79a777befc891b641
carloscursobr <carlosmatabr@gmail.com>
2026-06-02 06:13:50 -0300

    Update game.ts

--- src/types/game.ts
+++ src/types/game.ts
@@ -1,1 +1,30 @@
+export type HunterType = 1 | 2 | 3;
+export type StorageLevel = 1 | 2 | 3;
 
+export interface HunterConfig {
+  type: HunterType;
+  name: string;
+  icon: string;
+  productionPerHour: number;
+  costPlata: number;
+  costUsd: number;
+  attack: number;
+  defense: number;
+  hp: number;
+  requiredStorageLevel: StorageLevel;
+}
+
+export interface OwnedHunter {
+  uid: string;
+  type: HunterType;
+  purchasedAt: number;
+}
+
+export interface GameState {
+  plata: number;
+  storageLevel: StorageLevel;
+  storedArticles: number;
+  totalArticlesClaimed: number;
+  hunters: OwnedHunter[];
+  lastTick: number;
+}
